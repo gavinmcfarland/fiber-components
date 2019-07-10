134972a0990fee5c12b027aa08ef19ae3914666d
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-07-10 16:25:37 +0100

    first commit

---
+++ Section.svelte
@@ -0,0 +1,36 @@
+<script>
+	let className;
+	export { className as class };
+</script>
+
+<style>
+	.bg-fullbleed {
+		max-width: none !important;
+		width: 100vw;
+
+		position: relative;
+		left: 50%;
+		transform: translate(-50vw);
+	}
+
+	[class*="max-w"].bg-fullbleed {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	[class*="max-w"].bg-fullbleed > .wrapper {
+		max-width: var(--max-width);
+		display: block;
+		box-sizing: content-box;
+		width: 100vw;
+		flex-shrink: 0;
+		flex-basis: 100%;
+	}
+</style>
+
+<div class="{className}">
+	<div class="wrapper">
+		<slot></slot>
+	</div>
+</div>
