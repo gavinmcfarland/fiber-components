0d806e90a8f92acd9456229ced2807a8cb20d063
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-07-11 01:53:48 +0100

    updated section component with some styles

--- Section.svelte
+++ Section.svelte
@@ -1,36 +1,36 @@
 <script>
-	let className;
-	export { className as class };
+  let className;
+  export { className as class };
 </script>
 
 <style>
-	.bg-fullbleed {
-		max-width: none !important;
-		width: 100vw;
+  .w-viewport {
+    max-width: none !important;
+    width: 100vw;
 
-		position: relative;
-		left: 50%;
-		transform: translate(-50vw);
-	}
+    position: relative;
+    left: 50%;
+    transform: translate(-50vw);
+  }
 
-	[class*="max-w"].bg-fullbleed {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
+  [class*="max-w"].w-viewport {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-	[class*="max-w"].bg-fullbleed > .wrapper {
-		max-width: var(--max-width);
-		display: block;
-		box-sizing: content-box;
-		width: 100vw;
-		flex-shrink: 0;
-		flex-basis: 100%;
-	}
+  [class*="max-w"].w-viewport > .wrapper {
+    max-width: var(--max-width);
+    display: block;
+    box-sizing: content-box;
+    width: 100vw;
+    flex-shrink: 0;
+    flex-basis: 100%;
+  }
 </style>
 
-<div class="{className}">
-	<div class="wrapper">
-		<slot></slot>
-	</div>
+<div class="Section {className} max-w-6xl w-viewport border-t pt-12 pb-24">
+  <div class="wrapper">
+    <slot />
+  </div>
 </div>
